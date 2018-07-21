143430635fe6e99829b64016b4207806b5eb4512
TuanVo <voquoctuanqn@gmail.com>
2018-07-21 22:51:31 +0700

    [+] add file js of theme and  add file js init cookie order

---
+++ CMS-Web/Scripts/InitCookie.js
@@ -0,0 +1,91 @@
+﻿var CookieName = "cms-order";
+function CreateOrUpdateOrder(objOrder)
+{
+    var Orders = getListOrder();
+    if (Orders != null)
+    {
+        if (Orders.map(function (o) { return o.ItemId }).indexOf(objOrder.ItemId) == -1) {
+            // add order
+            Orders.push(objOrder);
+        }
+        else {
+            $.each(Orders, function (index, item) {
+                if (item.ItemId === objOrder.ItemId) {
+                    // update quantity
+                    item.Quantity += 1;
+                }
+            });
+        }
+
+        // remove cookie current
+        Cookies.remove(CookieName);
+        //add new cookie
+        Cookies.set(CookieName, Orders, { expires: 1 });
+    }
+    else {
+        var _order = [];
+        _order.push(objOrder);
+        Cookies.set(CookieName, _order, { expires: 1 })
+    }
+
+    //get list order again
+    Orders = getListOrder();
+}
+
+function getListOrder()
+{
+    var Orders = null;
+    var _Order = Cookies.getJSON(CookieName)
+    if (_Order != undefined)
+    {
+        Orders = _Order;
+        if (Orders !== undefined && Orders !== null) {
+            var _Quantity = Orders.length;
+            $('.notify-right').text(_Quantity);
+            $('.cart-items-count').text(_Quantity);
+        }
+    }
+    return Orders;
+}
+
+function deleteOrder() {
+    Cookies.remove(CookieName);
+    $('.notify-right').text(0);
+    $('.cart-items-count').text(0);
+}
+
+function UpdateQuantity(itemId, qty)
+{
+    var Orders = getListOrder();
+    if (Orders != null) {
+        $.each(Orders, function (index, item) {
+            if (item.ItemId === itemId) {
+                // update quantity
+                item.Quantity = qty;
+            }
+        });
+        // remove cookie current
+        Cookies.remove(CookieName);
+        //add new cookie
+        Cookies.set(CookieName, Orders, { expires: 1 });
+    }
+}
+
+function RemoveOrderByItemId(itemId)
+{
+    var Orders = getListOrder();
+    if (Orders != null)
+    {
+        var flgRemoveCookie = false;
+        for (var i = 0; i < Orders.length; i++) {
+            if (Orders[i].ItemId === itemId) {
+                Orders.splice(i, 1);
+                flgRemoveCookie = true;
+            }
+        }
+        if (flgRemoveCookie) {
+            Cookies.remove(CookieName);
+            Cookies.set(CookieName, Orders);
+        } 
+    }
+}
