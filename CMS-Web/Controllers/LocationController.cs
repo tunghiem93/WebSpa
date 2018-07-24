0673003f8977c1c3abafcdbde86adf8de976e64e
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-07-24 16:16:41 +0700

    Revert "[+] [M] add js, update source"
    This reverts commit 28e5a339e3e24d7bfa87292491be327f06b60738.

--- CMS-Web/Controllers/LocationController.cs
+++ CMS-Web/Controllers/LocationController.cs
@@ -1,6 +1,4 @@
-﻿using CMS_DTO.CMSLocation;
-using CMS_Shared.CMSDiscount;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,19 +6,12 @@ using System.Web.Mvc;
 
 namespace CMS_Web.Controllers
 {
-    public class LocationController : HQController
+    public class LocationController : Controller
     {
-        private CMSDiscountFactory _facDis;
-        public LocationController()
-        {
-            _facDis = new CMSDiscountFactory();
-        }
         // GET: Location
         public ActionResult Index()
         {
-            CMSLocationModels model = new CMSLocationModels();
-            model.LstDiscount = _facDis.GetList();
-            return View(model);
+            return View();
         }
     }
 }
