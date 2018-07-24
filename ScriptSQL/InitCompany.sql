fc576e2de8280f93c973a1ec562f130b85420c6c
Quang Tran <quangtrantpq@gmail.com>
2018-07-24 15:33:44 +0700

    [+] SQL: init db.

---
+++ ScriptSQL/InitCompany.sql
@@ -0,0 +1,42 @@
+﻿USE [dbLamode6]
+GO
+
+INSERT INTO [dbo].[CMS_Companies]
+           ([Id]
+           ,[Name]
+           ,[Description]
+           ,[Email]
+           ,[Phone]
+           ,[Address]
+           ,[LinkBlog]
+           ,[LinkFB]
+           ,[LinkTwiter]
+           ,[LinkInstagram]
+           ,[ImageURL]
+           ,[BusinessHour]
+           ,[IsActive]
+           ,[CreatedBy]
+           ,[UpdatedDate]
+           ,[UpdatedBy]
+           ,[CreatedDate])
+     VALUES
+           ('0fc574e0-3db9-4253-8fe9-9c3ecb1af2e1'
+           ,'Lamode Company'
+           ,'Always be the best.'
+           ,'lamode@gmail.com'
+           ,'123'
+           ,'181/29 Âu Dương Lân, P.2, Q8'
+		   ,''
+           ,''
+           ,''
+           ,''
+           ,''
+           ,''
+           ,1
+           ,'initial'
+           ,GETDATE()
+           ,'initial'
+           ,GETDATE())
+GO
+
+
