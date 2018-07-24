28e5a339e3e24d7bfa87292491be327f06b60738
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-07-24 15:18:34 +0700

    [+] [M] add js, update source

---
+++ CMS-DTO/CMSLocation/CMSLocationModels.cs
@@ -0,0 +1,18 @@
+﻿using CMS_DTO.CMSDiscount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_DTO.CMSLocation
+{
+    public class CMSLocationModels
+    {
+        public List<CMSDiscountModels> LstDiscount { get; set; }
+        public CMSLocationModels()
+        {
+            LstDiscount = new List<CMSDiscountModels>();
+        }
+    }
+}
