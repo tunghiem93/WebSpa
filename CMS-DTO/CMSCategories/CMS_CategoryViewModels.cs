e9f3913ab3ff9f3264b18dfe30848bc85696ef74
TuanVo <voquoctuanqn@gmail.com>
2018-07-24 11:41:34 +0700

    [M] update source

---
+++ CMS-DTO/CMSCategories/CMS_CategoryViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_DTO.CMSCategories
+{
+    public class CMS_CategoryViewModels
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int TotalProduct { get; set; }
+        public List<CMS_CategoryViewModels> Children { get; set; }
+        public CMS_CategoryViewModels()
+        {
+            Children = new List<CMS_CategoryViewModels>();
+        }
+    }
+}
--- CMS-DTO/CMSShop/CMS_ShopViewModels.cs
+++ CMS-DTO/CMSShop/CMS_ShopViewModels.cs
@@ -1,4 +1,5 @@
-﻿using CMS_DTO.CMSProduct;
+﻿using CMS_DTO.CMSCategories;
+using CMS_DTO.CMSProduct;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace CMS_DTO.CMSShop
     public class CMS_ShopViewModels
     {
         public List<CMS_ProductsModels> Products { get; set; }
+        public List<CMS_CategoryViewModels> Categories { get; set; }
         public int TotalPage { get; set; }
     }
 }
