ebef75800e9770d774258bdff399d54359e15c49
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-07-24 16:34:56 +0700

    [M] Update source

--- CMS-Web/Controllers/LocationController.cs
+++ CMS-Web/Controllers/LocationController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CMS_DTO.CMSLocation;
+using CMS_Shared.CMSDiscount;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,19 @@ using System.Web.Mvc;
 
 namespace CMS_Web.Controllers
 {
-    public class LocationController : Controller
+    public class LocationController : HQController
     {
+        private CMSDiscountFactory _facDis;
+        public LocationController()
+        {
+            _facDis = new CMSDiscountFactory();
+        }
         // GET: Location
         public ActionResult Index()
         {
-            return View();
+            CMSLocationModels model = new CMSLocationModels();
+            model.LstDiscount = _facDis.GetList();
+            return View(model);
         }
     }
 }
