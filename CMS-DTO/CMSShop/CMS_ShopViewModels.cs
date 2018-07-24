15047cbe38b9c08cab464ec0a436848754c8d43c
TuanVo <voquoctuanqn@gmail.com>
2018-07-24 10:05:49 +0700

    [M] update source

---
+++ CMS-DTO/CMSShop/CMS_ShopViewModels.cs
@@ -0,0 +1,15 @@
+﻿using CMS_DTO.CMSProduct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_DTO.CMSShop
+{
+    public class CMS_ShopViewModels
+    {
+        public List<CMS_ProductsModels> Products { get; set; }
+        public int TotalPage { get; set; }
+    }
+}
--- CMS-Web/Controllers/ShopController.cs
+++ CMS-Web/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CMS_DTO.CMSShop;
+using CMS_Shared.CMSProducts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,25 @@ namespace CMS_Web.Controllers
 {
     public class ShopController : Controller
     {
+        private CMSProductFactory _fac;
+        public ShopController()
+        {
+            _fac = new CMSProductFactory();
+        }
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            var models = new CMS_ShopViewModels();
+            try
+            {
+                var data = _fac.GetList();
+                models.Products = data;
+            }
+            catch(Exception ex)
+            {
+                NSLog.Logger.Error("Index Shop", ex);
+            }
+            return View(models);
         }
     }
 }
