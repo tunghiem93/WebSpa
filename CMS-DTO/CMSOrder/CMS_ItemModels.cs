56d91aed5ed8e41d4c7bcae846549a967c4078fe
TuanVo <voquoctuanqn@gmail.com>
2018-07-26 12:00:44 +0700

    [M] update source

---
+++ CMS-DTO/CMSOrder/CMS_ItemModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_DTO.CMSOrder
+{
+    public class CMS_ItemModels
+    {
+        public string ProductID { get; set; }
+        public double Quantity { get; set; }
+        public double Price { get; set; }
+        public string ProductName { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
--- CMS-Web/Controllers/ShopController.cs
+++ CMS-Web/Controllers/ShopController.cs
@@ -1,8 +1,10 @@
 ﻿using CMS_DTO.CMSCategories;
+using CMS_DTO.CMSOrder;
 using CMS_DTO.CMSShop;
 using CMS_Shared;
 using CMS_Shared.CMSCategories;
 using CMS_Shared.CMSProducts;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +13,7 @@ using System.Web.Mvc;
 
 namespace CMS_Web.Controllers
 {
-    public class ShopController : Controller
+    public class ShopController : HQController
     {
         private CMSProductFactory _fac;
         private CMSCategoriesFactory _facCate;
@@ -40,6 +42,45 @@ namespace CMS_Web.Controllers
                 NSLog.Logger.Error("Index Shop", ex);
             }
             return View(models);
+        }
+
+        public ActionResult Cart()
+        {
+            CMS_OrderViewModels model = new CMS_OrderViewModels();
+            try
+            {
+                var _Orders = GetListOrderCookie();
+                NSLog.Logger.Info("List Order Cookie", JsonConvert.SerializeObject(_Orders));
+                if(_Orders != null && _Orders.Any())
+                {
+                    var ItemIds = _Orders.Select(x => x.ItemId).ToList();
+                    var data = _fac.GetList().Where(o => ItemIds.Contains(o.Id))
+                                             .Select(o => new CMS_ItemModels
+                                             {
+                                                 Price = o.ProductPrice,
+                                                 ProductID = o.Id,
+                                                 ProductName = o.ProductName,
+                                                 Quantity = o.Quantity
+                                             }).ToList();
+                    if(data != null && data.Any())
+                    {
+                       data.ForEach(o =>
+                       {
+                           var item = _Orders.FirstOrDefault(z => z.ItemId.Equals(o.ProductID));
+                           o.Quantity = item.Quantity;
+                           o.TotalPrice = Convert.ToDouble(o.Price * item.Quantity);
+                       });
+                        model.ListItem = data;
+                        model.TotalPrice = data.Sum(o => o.TotalPrice);
+                        model.SubTotalPrice = data.Sum(o => o.TotalPrice);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                NSLog.Logger.Error("Cart", ex);
+            }
+            return View(model);
         }
     }
 }
