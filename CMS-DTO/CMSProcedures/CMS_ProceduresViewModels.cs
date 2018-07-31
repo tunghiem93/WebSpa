d180e44f85422504eb6e95b465e278d2e5dba640
Quang Tran <quangtrantpq@gmail.com>
2018-07-31 16:01:22 +0700

    [M] Procedure page: translate.

--- CMS-DTO/CMSProcedures/CMS_ProceduresViewModels.cs
+++ CMS-DTO/CMSProcedures/CMS_ProceduresViewModels.cs
@@ -15,36 +15,36 @@ namespace CMS_DTO.CMSProcedures
             ListProcedures = new List<CMS_ProceduresModels>() {
                 new CMS_ProceduresModels()
                 {
-                    ShortDescription = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias provident destiny is about voles.",
+                    ShortDescription = "Trong cuộc sống nhộn nhịp với bao lo toan, các chị em phụ nữ nên dành cho mình ít thời gian massage thư giãn làm đẹp, đồng thời massage cũng  kích thích các nguyệt đạo cơ thể giúp giảm stress cũng như làm lưu thông mạch máu cơ thể.",
                     ImageUrl =CMS_Common.Commons._PublicImages+ "Procedures/popular-procedures1.jpg",
                     ProceduresName = "Massage",
                     Preparation = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.",
                     Process = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
                     SpaTreatment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-                    Duration = "1-2hr",
-                    Price = 18000,
+                    Duration = "1-2 giờ",
+                    Price = 180000,
                 },
                 new CMS_ProceduresModels()
                 {
-                    ShortDescription = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias provident destiny is about voles.",
+                    ShortDescription = "Thanh lọc da tiêu chuẩn, Cung cấp sức sống cho làn da, Liệu pháp giảm căng thẳng bằng hương thơm, Chăm sóc da cơ bản bằng vitamin C, Chăm sóc da khô và lão hóa bằng vitamin C, Chăm sóc da bằng thuốc bắc, Chăm sóc da bằng mật ong sữa chua.",
                     ImageUrl =CMS_Common.Commons._PublicImages+ "Procedures/popular-procedures2.jpg",
                     ProceduresName = "Chăm sóc da",
                     Preparation = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.",
                     Process = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
                     SpaTreatment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-                    Duration = "1-2hr",
-                    Price = 20000,
+                    Duration = "1-2 giờ",
+                    Price = 200000,
                 },
                 new CMS_ProceduresModels()
                 {
-                    ShortDescription = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias provident destiny is about voles.",
+                    ShortDescription = "Thảo dược là một trong những bí quyết làm đẹp an toàn, hiệu quả đến từ tự nhiên. Thành phần khoáng chất, dinh dưỡng chứa trong thảo dược giúp duy trì độ săn chắc của làn da, giúp tái tạo các tế bào mới, cho làn da tươi sáng và khoẻ mạnh hơn.",
                     ImageUrl = CMS_Common.Commons._PublicImages + "Procedures/popular-procedures3.jpg",
                     ProceduresName = "Liệu trình thảo dược",
                     Preparation = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.",
                     Process = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
                     SpaTreatment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-                    Duration = "1-2hr",
-                    Price = 30000,
+                    Duration = "1-2 giờ",
+                    Price = 300000,
                 },
                 new CMS_ProceduresModels()
                 {
@@ -54,8 +54,8 @@ namespace CMS_DTO.CMSProcedures
                     Preparation = "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.",
                     Process = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
                     SpaTreatment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit elit turpis, a porttitor tellus sollicitudin at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-                    Duration = "1-2hr",
-                    Price = 18000,
+                    Duration = "1-2 giờ",
+                    Price = 180000,
                 }
 
             };
