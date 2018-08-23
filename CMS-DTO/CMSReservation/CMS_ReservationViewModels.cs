cf1c81f407d7c6e28e664e1455ccbdaab3f64680
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-08-23 11:19:27 +0700

    [+] and [M] source

--- CMS-DTO/CMSReservation/CMS_ReservationViewModels.cs
+++ CMS-DTO/CMSReservation/CMS_ReservationViewModels.cs
@@ -1,7 +1,9 @@
 ﻿using CMS_DTO.CMSCategories;
+using CMS_DTO.CMSCustomer;
 using CMS_DTO.CMSEmployee;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +12,33 @@ namespace CMS_DTO.CMSReservation
 {
     public class CMS_ReservationViewModels
     {
-        public List<CMSCategoriesModels> Categories { get; set; }
-        public List<CMS_EmployeeModels> Employees { get; set; }
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập e-mail")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail không hợp lệ")]
+        public string Email { get; set; }
+        [RegularExpression("([0-9]+)", ErrorMessage = "Vui lòng nhập số")]
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BookDay { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string sStatus { get; set; }
+        public List<CustomerModels> ListCustomer { get; set; }
+        public List<CMSCategoriesModels> ListCategories { get; set; }
+        public List<CMS_EmployeeModels> ListEmployees { get; set; }
 
         public CMS_ReservationViewModels()
         {
-            Categories = new List<CMSCategoriesModels>();
-            Employees = new List<CMS_EmployeeModels>();
+            ListCustomer = new List<CustomerModels>();
+            ListCategories = new List<CMSCategoriesModels>();
+            ListEmployees = new List<CMS_EmployeeModels>();
         }
     }
 }
--- CMS-Web/Controllers/ReservationController.cs
+++ CMS-Web/Controllers/ReservationController.cs
@@ -1,5 +1,6 @@
 ﻿using CMS_DTO.CMSReservation;
 using CMS_Shared.CMSCategories;
+using CMS_Shared.CMSCustomers;
 using CMS_Shared.CMSEmployees;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,13 @@ namespace CMS_Web.Controllers
     {
         private CMSCategoriesFactory _facCate;
         private CMSEmployeeFactory _facEmp;
+        private CMSCustomersFactory _facCus;
 
         public ReservationController()
         {
             _facCate = new CMSCategoriesFactory();
             _facEmp = new CMSEmployeeFactory();
+            _facCus = new CMSCustomersFactory();
         }
         // GET: Reservation
         public ActionResult Index()
@@ -25,8 +28,9 @@ namespace CMS_Web.Controllers
             var model = new CMS_ReservationViewModels();
             try
             {
-                model.Categories = _facCate.GetList().Where(o => o.ProductTypeCode == (int)CMS_Common.Commons.EProductType.Procudure).ToList();
-                model.Employees = _facEmp.GetList();
+                model.ListCustomer = _facCus.GetList();
+                model.ListCategories = _facCate.GetList().Where(o => o.ProductTypeCode == (int)CMS_Common.Commons.EProductType.Procudure).ToList();
+                model.ListEmployees = _facEmp.GetList();
             }
             catch (Exception ex)
             {
